b9caffc2f559e2e918d52729c460303a9457f540
capu <hi@capu.tech>
2020-09-24 16:36:13 -0300

    persisting shareRateChange

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,6 +1,7 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import {
   ShareRateChange as ShareRateChangeEvent,
+  Contract
 } from "../generated/Contract/Contract"
 import { GlobalState, ShareRateChange } from "../generated/schema"
 
@@ -12,15 +13,25 @@ function getOrCreateGlobalState(): GlobalState {
   let state = GlobalState.load(GLOBAL_STATE_ID);
   if(state == null) {
     state = new GlobalState(GLOBAL_STATE_ID)
+    state.shareRateRaw = new BigInt(0);
   }
   return state as GlobalState
 }
 
 export function handleShareRateChange(event: ShareRateChangeEvent): void {
   let state: GlobalState = getOrCreateGlobalState();
+  let contract :Contract = Contract.bind(event.address);
   // get rid of manually-packed block timestamp
-  let shareRateRaw: String  = event.params.data0.toHex();
+  let globals: Array<BigInt>  = contract.globalInfo();
+  let shareRateRaw: BigInt = globals[2];
 
+  let stakeId : String = event.params.stakeId.toString();
+  // TODO: check if there can be multiple ShareRateChange events for a single stakeId
+  let shareRateChange: ShareRateChange = new ShareRateChange(stakeId);
+  shareRateChange.stakeId = stakeId;
+  shareRateChange.oldShareRateRaw = state.shareRateRaw;
+  shareRateChange.newShareRateRaw = shareRateRaw;
   state.shareRateRaw = shareRateRaw;
-  state.save()
+  shareRateChange.save();
+  state.save();
 }
