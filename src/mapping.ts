058199daaed394b0b828ec297c7fb2d3979d25d7
capu <hi@capu.tech>
2020-09-24 16:55:17 -0300

    persisting all of the global state

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,4 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, ethereum } from "@graphprotocol/graph-ts"
 import {
   ShareRateChange as ShareRateChangeEvent,
   Contract
@@ -18,20 +18,41 @@ function getOrCreateGlobalState(): GlobalState {
   return state as GlobalState
 }
 
-export function handleShareRateChange(event: ShareRateChangeEvent): void {
+function updateGlobalState(event: ethereum.Event): void {
   let state: GlobalState = getOrCreateGlobalState();
   let contract :Contract = Contract.bind(event.address);
-  // get rid of manually-packed block timestamp
-  let globals: Array<BigInt>  = contract.globalInfo();
-  let shareRateRaw: BigInt = globals[2];
+  let globals: Array<BigInt> =  contract.globalInfo();
 
+  state.lockedHeartsTotalRaw       = globals[0];
+  state.nextStakeSharesTotalRaw    = globals[1];
+  state.shareRateRaw               = globals[2];
+  state.stakePenaltyTotalRaw       = globals[3];
+  state.dailyDataCountRaw          = globals[4];
+  state.stakeSharesTotalRaw        = globals[5];
+  state.latestStakeIdRaw           = globals[6];
+  state.unclaimedSatoshisTotalRaw = globals[7];
+  state.claimedSatoshisTotalRaw   = globals[8];
+  state.claimedBtcAddrCountRaw    = globals[9];
+  // skip block timestamp
+  state.totalSupplyRaw             = globals[11];
+  state.xfLobbyForCurrentDayRaw    = globals[12];
+
+  state.save();
+}
+
+export function handleShareRateChange(event: ShareRateChangeEvent): void {
+  let oldShareRateRaw: BigInt = getOrCreateGlobalState().shareRateRaw;
+  updateGlobalState(event);
+  let shareRateRaw: BigInt = getOrCreateGlobalState().shareRateRaw;
   let stakeId : String = event.params.stakeId.toString();
   // TODO: check if there can be multiple ShareRateChange events for a single stakeId
   let shareRateChange: ShareRateChange = new ShareRateChange(stakeId);
+
   shareRateChange.stakeId = stakeId;
-  shareRateChange.oldShareRateRaw = state.shareRateRaw;
+  shareRateChange.oldShareRateRaw = oldShareRateRaw;
   shareRateChange.newShareRateRaw = shareRateRaw;
-  state.shareRateRaw = shareRateRaw;
+  shareRateChange.differenceRaw = shareRateRaw - oldShareRateRaw;
+  shareRateChange.blockNumber = event.block.number;
+
   shareRateChange.save();
-  state.save();
 }
